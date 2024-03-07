0d46a710f447dcc033b2561b30637c4d3487b274
liamd <liam.daigle@gmail.com>
2024-03-06 23:32:55 -0500

    Added Basic Backend Structure

---
+++ SmartHomeSystem/backend/src/main/java/com/smarthome/smarthomesystem/domain/User.java
@@ -0,0 +1,26 @@
+package com.smarthome.smarthomesystem.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id_seq")
+    private Long id;
+
+    private String username;
+
+    private String password;    //Change this in the later stages to have salt + hash + pepper
+
+
+}
---
+++ SmartHomeSystem/backend/src/test/java/com/smarthome/smarthomesystem/SmartHomeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarthome.smarthomesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
