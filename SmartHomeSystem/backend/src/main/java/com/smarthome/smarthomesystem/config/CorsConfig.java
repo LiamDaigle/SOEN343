d5ec8f250e8859e9cdaf7a8475d9144636eaaeb0
Ahmad-Elmahallawy <ahmad.mahallawy@gmail.com>
2024-03-11 03:07:04 -0400

    added Cors configuration in backend

--- SmartHomeSystem/backend/src/main/java/com/smarthome/smarthomesystem/config/CorsConfig.java
+++ SmartHomeSystem/backend/src/main/java/com/smarthome/smarthomesystem/config/CorsConfig.java
@@ -1,2 +1,16 @@
-package com.smarthome.smarthomesystem.config;public class CorsConfig {
+package com.smarthome.smarthomesystem.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // Update with your frontend origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
 }
