47c957917498e98339d8ac54c9ecd1e0b69a8126
Vanessa DiPietrantonio <vanessadpwong17@gmail.com>
2024-03-13 22:36:21 -0400

    updated House Layout

--- SmartHomeSystem/backend/src/main/resources/data.sql
+++ SmartHomeSystem/backend/src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO rooms (id,name) VALUES (0,'Backyard');
-INSERT INTO rooms (id,name) VALUES (1,'Garage');
-INSERT INTO rooms (id,name) VALUES (2,'Entrance');
-INSERT INTO rooms (id,name) VALUES (3,'Bedroom');
-INSERT INTO rooms (id,name) VALUES (4,'LivingRoom');
+INSERT INTO rooms (id,name) VALUES (1,'Entrance');
+INSERT INTO rooms (id,name) VALUES (2,'Garage');
+INSERT INTO rooms (id,name) VALUES (3,'LivingRoom');
+INSERT INTO rooms (id,name) VALUES (4,'Bedroom');
 
 INSERT INTO doors (id, is_auto_lock, is_open, room_id) VALUES (0,false,true,0);
 INSERT INTO doors (id, is_auto_lock, is_open, room_id) VALUES (1,false,true,0);
@@ -13,8 +13,8 @@ INSERT INTO doors (id, is_auto_lock, is_open, room_id) VALUES (5,false,true, 4);
 INSERT INTO doors (id, is_auto_lock, is_open, room_id) VALUES (6,false,true, 4);
 
 INSERT INTO windows (id, is_open, is_blocked, room_id) VALUES (0, false, false, 0);
-INSERT INTO windows (id, is_open, is_blocked, room_id) VALUES (1, false, false, 0);
-INSERT INTO windows (id, is_open, is_blocked, room_id) VALUES (2, false, false, 1);
+INSERT INTO windows (id, is_open, is_blocked, room_id) VALUES (1, false, false, 1);
+INSERT INTO windows (id, is_open, is_blocked, room_id) VALUES (2, false, false, 2);
 INSERT INTO windows (id, is_open, is_blocked, room_id) VALUES (3, false, false, 3);
 INSERT INTO windows (id, is_open, is_blocked, room_id) VALUES (4, false, false, 4);
 
